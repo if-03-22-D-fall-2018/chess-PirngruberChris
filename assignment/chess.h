de7fe224f1aa3dd8225dab847cde1df5cdd0f5c3
PirngruberChris <christianpirngruber3@gmail.com>
2018-11-04 11:06:46 +0100

    going on some errors left

--- assignment/chess.h
+++ assignment/chess.h
@@ -12,21 +12,41 @@
  */
 
 
-enum color { White, Black };
-enum PieceType{Pawn, Knight, Rook, Bishop, Queen, King};
+enum Color { White, Black };
+enum PieceType{Pawn, Knight, Rook, Bishop, Queen, King, NoPiece};
 
 struct ChessPiece{
   enum Color color;
-  enum PieceType piece;
+  enum PieceType type;
 };
 
  struct ChessSquare{
    bool is_occupied;
-   struct ChessPiece;
+   struct ChessPiece piece;
  };
 
  typedef ChessSquare ChessBoard[8][8];
+ typedef char File;
+ typedef int Rank;
 
 bool is_piece(struct ChessPiece piece, enum PieceType type, enum Color color);
 
 void init_chess_board(ChessBoard chess_board);
+
+bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type);
+
+ChessSquare* get_square(ChessBoard chess_board, File file, Rank rank);
+
+bool is_square_occupied(ChessBoard chess_board, File file, Rank rank);
+
+void init_chess_board(ChessBoard chess_board);
+
+bool add_piece(ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece);
+
+ChessPiece get_piece(ChessBoard chess_board, File file, Rank rank);
+
+void setup_chess_board(ChessBoard chess_board);
+
+bool remove_piece(ChessBoard chess_board, File file, Rank rank);
+
+bool squares_share_file(File file, Rank rank, File second_file, Rank second_rank);
